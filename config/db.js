2694cc88d0745cde2f59a9e54e4b4f84f8bb0c7a
wadhah hermessi <wadhah.hermessi@enicar.ucar.tn>
2026-04-23 11:12:43 +0100

    safer password management

--- config/db.js
+++ config/db.js
@@ -5,8 +5,10 @@ const pool = new Pool({
     user: process.env.DB_USER || 'postgres',
     host: process.env.DB_HOST || 'localhost',
     database: process.env.DB_NAME || 'smartfarm',
-    password: process.env.DB_PASSWORD || 'password',
+    password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT || 5432,
 });
-
+pool.connect()
+    .then(() => console.log("PostgreSQL connected"))
+    .catch(err => console.error("DB connection error", err));
 module.exports = pool;
